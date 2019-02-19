12d78e25e7362de934211b222e919fcdb35700bf
clRcoDE <45456252+clRcoDE@users.noreply.github.com>
2019-02-19 17:20:00 +0330

    different apporoches for filtering data

--- src/App.js
+++ src/App.js
@@ -1,28 +1,164 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import {  Image, StyleSheet, Text, View ,Dimensions, TouchableHighlight} from "react-native";
+
 
 class App extends Component {
+
+constructor(props){
+  super(props);
+  this.state={
+
+  }
+  let buttons = [
+    {
+        id: 'hotels',
+        
+    },
+    {
+        id: 'flights',
+    },
+    {
+        id: 'rails',
+    }
+]
+}
+
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <View  style={styles.conrainer}>
+
+<View>
+  this.buttons.map(()=>)
+  <TouchableHighlight><View></View></TouchableHighlight>
+</View>
+
+      </View>
     );
   }
 }
+
+
+const styles=StyleSheet.create({
+  conrainer:{
+    flex:1,
+    backgroundColor:'royalblue',
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+})
+
+
+const Data = [
+  {
+
+      startTime: '02 Jul 2019',
+      fromCity: 'London',
+      toCity: 'San Francisco',
+      fromCountry: 'UK',
+      toCountry: 'USA',
+      TakeOff: '10:30',
+      Landing: '22:10',
+      StartCityAbbreviation: 'LDN',
+      DestinationCityAbbreviation: 'SFR',
+      class: 'Business',
+      MealPlan: 'Standard',
+      Seat: 'Window',
+      // Airline: { name: 'Emirates', icon: require('../Assets/Images/emirates.png') },
+      tag:'flights'
+
+
+  },
+  {
+
+      startTime: '18 Jun 2019',
+      endTime:'11 Sep 2019',
+      fromCity: 'Delhi',
+      toCity: 'Moscow',
+      fromCountry: 'India',
+      toCountry: 'Russia',
+      TakeOff: '18:30',
+      Landing: '23:30',
+      StartCityAbbreviation: 'DEL',
+      DestinationCityAbbreviation: 'MCW',
+      class: 'First Class',
+      MealPlan: 'Vegetables',
+      Seat: 'beside',
+      // Airline: { name: 'Qatar Airways', icon: require('../Assets/Images/qatarAirways.png') },
+      tag:'rails'
+
+
+  },
+
+
+  {
+
+      startTime: '22 Apr 2019',
+      endTime:'11 Sep 2019',
+
+      fromCity: 'London',
+      toCity: 'San Francisco',
+      fromCountry: 'UK',
+      toCountry: 'USA',
+      TakeOff: '10:30',
+      Landing: '22:10',
+      StartCityAbbreviation: 'LDN',
+      DestinationCityAbbreviation: 'SFR',
+      class: 'Business',
+      MealPlan: 'diet',
+      Seat: 'Window',
+      // Airline: { name: 'virgin atlantic', icon: require('../Assets/Images/virgin.jpg') },
+      tag:'hotels'
+
+
+  },
+
+  {
+
+      startTime: '14 Feb 2019',
+      fromCity: 'London',
+      toCity: 'San Francisco',
+      fromCountry: 'UK',
+      toCountry: 'USA',
+      TakeOff: '10:30',
+      Landing: '22:10',
+      StartCityAbbreviation: 'LDN',
+      DestinationCityAbbreviation: 'SFR',
+      class: 'Business',
+      MealPlan: 'Standard',
+      Seat: 'Window',
+      // Airline: { name: 'Emirates', icon: require('../Assets/Images/emirates.png') },
+      tag:'rails'
+
+
+  },
+
+  {
+
+      startTime: '02 Jul 2019',
+      endTime:'11 Sep 2019',
+
+      fromCity: 'London',
+      toCity: 'San Francisco',
+      fromCountry: 'United Kingdom',
+      toCountry: 'USA',
+      TakeOff: '10:30',
+      Landing: '22:10',
+      StartCityAbbreviation: 'LDN',
+      DestinationCityAbbreviation: 'SFR',
+      class: 'Business',
+      MealPlan: 'Standard',
+      Seat: 'Window',
+      // Airline: { name: 'Qatar Airways', icon: require('../Assets/Images/qatarAirways.png') },
+      tag:'flights'
+
+
+  },
+
+
+
+
+]
 
 export default App;
